8a55413732061c403e1f8e6f66744ba1e3912871
arian <arianpourinfo@gmail.com>
2024-08-30 22:56:28 +0800

    Initial commit

---
+++ src/main/java/com/amanatpay/onramp/OnrampApplication.java
@@ -0,0 +1,13 @@
+package com.amanatpay.onramp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnrampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnrampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amanatpay/onramp/OnrampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amanatpay.onramp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnrampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
